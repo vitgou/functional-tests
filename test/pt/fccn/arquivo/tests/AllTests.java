e08d1da97a86b32a014ffc8fbe0e713c307da0f8
AWP <awp@asa.fccn.pt>
2016-05-30 14:03:56 +0100

    added functional tests to the new Replay Page

--- test/pt/fccn/arquivo/tests/AllTests.java
+++ test/pt/fccn/arquivo/tests/AllTests.java
@@ -27,7 +27,7 @@ import org.junit.runners.Suite.SuiteClasses;
  */
 @RunWith(Suite.class)
 @SuiteClasses({ TestSearchOneTerm.class,HighlightsTest.class, 
-        TermsAndConditionsTest.class, TestSponsorImage.class, TestSearchOneTermOpenSearch.class,TestArcproxy.class,AdvancedTest.class,UrlsearchTest.class })
+        TermsAndConditionsTest.class, TestSponsorImage.class, TestSearchOneTermOpenSearch.class,TestArcproxy.class,AdvancedTest.class,UrlsearchTest.class, ReplayTest.class })
 public class AllTests {
 
 }
