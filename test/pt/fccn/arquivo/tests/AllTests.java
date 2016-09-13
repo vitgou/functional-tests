5f4ff955ccbb9239b370368a640884eac0e20d49
AWP <awp@asa.fccn.pt>
2016-09-13 16:34:54 +0100

    updating saucelabs remote and paralell

--- test/pt/fccn/arquivo/tests/AllTests.java
+++ test/pt/fccn/arquivo/tests/AllTests.java
@@ -29,7 +29,7 @@ import org.junit.runners.Suite.SuiteClasses;
 /*@SuiteClasses({ TestSearchOneTerm.class,HighlightsTest.class, 
         TermsAndConditionsTest.class, TestSponsorImage.class, TestSearchOneTermOpenSearch.class,TestArcproxy.class,AdvancedTest.class,UrlsearchTest.class, ReplayTest.class })*/
 @SuiteClasses({ TestSearchOneTerm.class,HighlightsTest.class, 
-    TermsAndConditionsTest.class, TestSponsorImage.class, TestSearchOneTermOpenSearch.class,TestArcproxy.class,AdvancedTest.class, /*ReplayTest.class,*/ UrlsearchTest.class })
+    TermsAndConditionsTest.class, TestSponsorImage.class, TestSearchOneTermOpenSearch.class,TestArcproxy.class,AdvancedTest.class, ReplayTest.class, UrlsearchTest.class })
 
 public class AllTests {
 
