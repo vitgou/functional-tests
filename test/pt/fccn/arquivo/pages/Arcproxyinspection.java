1234313c5387322bf42fd2198a10ef50e0f1b953
AWP <awp@asa.fccn.pt>
2016-09-22 17:49:50 +0100

    Fixing ArcproxyTest for mobile

--- test/pt/fccn/arquivo/pages/Arcproxyinspection.java
+++ test/pt/fccn/arquivo/pages/Arcproxyinspection.java
@@ -29,7 +29,7 @@ import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 import java.util.NoSuchElementException;
-import java.lang.RuntimeException
+import java.lang.RuntimeException;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
