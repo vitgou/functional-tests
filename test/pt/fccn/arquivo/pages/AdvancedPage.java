30602c711294e6b7e732332eb2df0ca884908cdb
AWP <awp@asa.fccn.pt>
2016-09-27 14:13:25 +0100

    Fixing HighLightsTest for mobile

--- test/pt/fccn/arquivo/pages/AdvancedPage.java
+++ test/pt/fccn/arquivo/pages/AdvancedPage.java
@@ -77,8 +77,12 @@ public class AdvancedPage {
             driver.findElement(By.id("adv_and")).sendKeys("sapo");
             driver.findElement(By.id("site")).clear();
             driver.findElement(By.id("site")).sendKeys("sapo.pt");
-            
             driver.findElement(By.id("btnSubmitBottom")).click();
+            try {
+                Thread.sleep(5000);                 //wait for page to load
+            } catch(InterruptedException ex) {
+                Thread.currentThread().interrupt();
+            }
             WebElement listOfResults = driver.findElement(By.id(listOfResultsTag));
             
             title=listOfResults.findElement(By.xpath("//*[@id=\"resultados-lista\"]/ul/li[1]/h2")).getText();
