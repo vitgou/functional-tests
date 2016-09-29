0f696377b3668c1ef0fd4831d8dead2721b85850
AWP <awp@asa.fccn.pt>
2016-09-29 11:52:39 +0100

    Fixing HighLightsTest for mobile

--- test/pt/fccn/arquivo/pages/HighlightsPage.java
+++ test/pt/fccn/arquivo/pages/HighlightsPage.java
@@ -218,7 +218,7 @@ public class HighlightsPage {
 	private String  getIdTitle (String Url){
 		WebDriverWait wait = new WebDriverWait(driver, 20);
 
-
+		System.out.println("Going to URL: " + Url);
 		driver.get(Url);
 		//wait until title was loaded
 		try {
