253c85a45b36bef65174362f48d8c818efd2d39c
AWP <awp@asa.fccn.pt>
2016-10-26 17:47:32 +0100

    SearchPage changed timeout getFirstResult()

--- test/pt/fccn/arquivo/tests/TestSearchOneTerm.java
+++ test/pt/fccn/arquivo/tests/TestSearchOneTerm.java
@@ -29,8 +29,11 @@ public class TestSearchOneTerm extends WebDriverTestBaseParalell {
         System.out.print("Running testSearchOneTerm. \n");
         SearchPage searchResults = index.search(term);
         titleOfFirstResult=searchResults.getFirstResult();
+        System.out.println("testSearchOneTerm 1");
         assertTrue("The search term was not found in results", searchResults.titleIsCorrect(term));
+        System.out.println("testSearchOneTerm 2");
         assertTrue("The search did not return results", searchResults.existsInResults(term));
+        System.out.println("testSearchOneTerm 3");
         assertTrue("The spellchecker is not working",searchResults.spellcheckerOK(testerSpellChecker));
         /*assertTrue("The replay bar is not working",searchResults.testReplayBar()); Commented this test. Only Works For Desktops, should be in a different test*/
     }
