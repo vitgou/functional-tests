9a6d02f1a7d8b2c0e81038eafb68b8025cc680bc
AWP <awp@asa.fccn.pt>
2016-10-28 17:00:36 +0100

    Fixing termsAndConditionsTest

--- test/pt/fccn/arquivo/pages/TermsAndConditionsPage.java
+++ test/pt/fccn/arquivo/pages/TermsAndConditionsPage.java
@@ -56,6 +56,9 @@ public class TermsAndConditionsPage {
     public boolean toEnglishVersion(){
         // Check that we're on the right page.
         System.out.println("TO ENGLISH");
+        System.out.println("Texto: " + (new WebDriverWait(driver, 25)) /* Wait Up to 25 seconds should throw RunTimeExcpetion*/
+            .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id='portal-languageselector']/li[1]/a"))).getText());
+
         WebElement linkTextENElement = (new WebDriverWait(driver, 25)) /* Wait Up to 25 seconds should throw RunTimeExcpetion*/
             .until(ExpectedConditions.presenceOfElementLocated(By.linkText(linkTextEN))); 
         linkTextENElement.click();
