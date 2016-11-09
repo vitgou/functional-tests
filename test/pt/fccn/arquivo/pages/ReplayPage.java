2cefb3b6177dca2704212dd639bc21e5fbd5a528
AWP <awp@asa.fccn.pt>
2016-11-09 15:33:34 +0000

    Fixing  ReplayTest

--- test/pt/fccn/arquivo/pages/ReplayPage.java
+++ test/pt/fccn/arquivo/pages/ReplayPage.java
@@ -26,7 +26,7 @@ import java.util.Iterator;
 import java.util.List;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.interactions.Actions
+import org.openqa.selenium.interactions.Actions;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
