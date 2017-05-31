360c7b47ad81b9264acb1612be8b45f11ad590cd
jnobre <joaoanobre@gmail.com>
2017-05-31 11:40:29 +0100

    remove suite  TermsAndConditionsTest

--- test/pt/fccn/arquivo/tests/AllTests.java
+++ test/pt/fccn/arquivo/tests/AllTests.java
@@ -29,8 +29,8 @@ import org.junit.runners.Suite.SuiteClasses;
 /*@SuiteClasses({ TestSearchOneTerm.class,HighlightsTest.class, 
         TermsAndConditionsTest.class, TestSponsorImage.class, TestSearchOneTermOpenSearch.class,TestArcproxy.class,AdvancedTest.class,UrlsearchTest.class, ReplayTest.class })*/
 // HighlightsTest.class 
-@SuiteClasses({ TestSearchOneTerm.class, ReplayTest.class,
-    TermsAndConditionsTest.class, TestSponsorImage.class, TestSearchOneTermOpenSearch.class,TestArcproxy.class,AdvancedTest.class, UrlsearchTest.class }) /*TODO:: Add replayTest*/
+@SuiteClasses({ TestSearchOneTerm.class, ReplayTest.class, TestSponsorImage.class, TestSearchOneTermOpenSearch.class,TestArcproxy.class,AdvancedTest.class, UrlsearchTest.class }) 
+//TODO refactoring TermsAndConditionsTest.class
 
 public class AllTests {
 
