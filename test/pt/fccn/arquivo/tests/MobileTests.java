fe59314a4bcc2ea71b3e4cebeaf099d0ff7f7ea2
jnobre <joaoanobre@gmail.com>
2017-05-31 11:59:21 +0100

    (Mobile) remove suite TermsAndConditionsTest

--- test/pt/fccn/arquivo/tests/MobileTests.java
+++ test/pt/fccn/arquivo/tests/MobileTests.java
@@ -28,8 +28,8 @@ import org.junit.runners.Suite.SuiteClasses;
 @RunWith(Suite.class)
 /*@SuiteClasses({ TestSearchOneTerm.class,HighlightsTest.class, 
         TermsAndConditionsTest.class, TestSponsorImage.class, TestSearchOneTermOpenSearch.class,TestArcproxy.class,AdvancedTest.class,UrlsearchTest.class, ReplayTest.class })*/
-@SuiteClasses({ TestSearchOneTerm.class, 
-    TermsAndConditionsTest.class, TestSponsorImage.class, TestSearchOneTermOpenSearch.class,AdvancedTest.class, UrlsearchTest.class })
+@SuiteClasses({ TestSearchOneTerm.class, TestSponsorImage.class, TestSearchOneTermOpenSearch.class,AdvancedTest.class, UrlsearchTest.class })
+//TODO refactoring and add suite TermsAndConditionsTest.class
 
 public class MobileTests {
 
