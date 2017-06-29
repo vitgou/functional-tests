a2f6d838c21f121d43484cb9a103f52940eb4939
jnobre <joaoanobre@gmail.com>
2017-06-29 15:59:50 +0100

    [PublicationsPage] work progress..

--- test/pt/fccn/sobre/arquivo/pages/IndexSobrePage.java
+++ test/pt/fccn/sobre/arquivo/pages/IndexSobrePage.java
@@ -23,6 +23,8 @@ import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import pt.fccn.arquivo.tests.util.AnalyzeURLs;
+
 
 
 
@@ -185,6 +187,11 @@ public class IndexSobrePage {
     
     }
     
+    /**
+     * 
+     * @param language
+     * @return
+     */
     public boolean checkFooterLinks( String language ) {
 		System.out.println( "[checkFooterLinks]" );
     	String xpatha = "//*[@id=\"footer-widgets\"]/div/div/div/aside/ul/li/a"; //get footer links
@@ -207,10 +214,12 @@ public class IndexSobrePage {
     			if( url.startsWith( "http://www.facebook.com/" ) ){
     				if( !textTolink.get( text ).equals( url ) )
     					return false;
-    			} else 
-    				if( !linkExists( url , text ) )
-	    				return false;
-    				//Debug System.out.println( "Return = " + linkExists( url , text ) );
+    			} else {
+    				if( !AnalyzeURLs.linkExists( url , text , textTolink ) )
+    					return false;
+	    			
+    			}
+    			//Debug System.out.println( "Return = " + linkExists( url , text ) );
     		
     		}
     		
@@ -224,81 +233,11 @@ public class IndexSobrePage {
     
     }
     
-    
-	/**
-	 * Check if link exists
-	 * @param URLName
-	 * @return
-	 */
-	private boolean linkExists( String URLName , String text  ) {
-	    boolean redirect = false;
-		try {
-	    	System.out.println( "[Footer] url[" + URLName + "]" );
-	    	
-	    	HttpURLConnection con = ( HttpURLConnection ) new URL( URLName ).openConnection( );
-	    	con.setConnectTimeout( 5000 );
-	    	con.setRequestMethod( "HEAD" );
-	    	con.addRequestProperty( "Accept-Language", "en-US,en;q=0.8" );
-	    	con.addRequestProperty( "User-Agent", "Mozilla" );
-	    	con.addRequestProperty( "Referer", "google.com" );
-	    	
-	    	// normally, 3xx is redirect
-	    	int status = con.getResponseCode( );
-	    	System.out.println( "Status-code = " + status );
-	    	if (status != HttpURLConnection.HTTP_OK) {
-	    		if (status == HttpURLConnection.HTTP_MOVED_TEMP
-	    			|| status == HttpURLConnection.HTTP_MOVED_PERM
-	    				|| status == HttpURLConnection.HTTP_SEE_OTHER)
-	    		redirect = true;
-	    	}
-	    	
-	    	redirect = checkRedirect( status );
-	    	
-	    	System.out.println( "[Footer] url[" + URLName + "] Status-code = " + con.getResponseCode( ) );
-	    	con.disconnect( );
-	    	
-	    	while( redirect ) {
-	    		// get redirect url from "location" header field
-	    		String newUrl = con.getHeaderField( "Location" );
-	    		System.out.println( "Redirect: true url["+URLName+"] newurl["+newUrl+"]" );
-	    		// get the cookie if need, for login
-	    		String cookies = con.getHeaderField( "Set-Cookie" );
-	    		
-	    		// open the new connection again
-				con = ( HttpURLConnection ) new URL( newUrl ).openConnection( );
-				con.setConnectTimeout( 5000 );
-				con.setRequestMethod( "HEAD" );
-	    		con.setRequestProperty( "Cookie", cookies );
-
-		    	
-		    			con.addRequestProperty( "Accept-Language", "en-US,en;q=0.8" );
-	    		con.addRequestProperty( "User-Agent", "Mozilla" );
-	    		con.addRequestProperty( "Referer", "google.com" );
-	    		status = con.getResponseCode( );
-
-	    		URLName = newUrl;
-	    		System.out.println( "Novo redirect status = " + status + " message = " + con.getResponseMessage( ) );
-	    		redirect = checkRedirect( status );
-	    		con.disconnect( );
-	    	}
-	    	
-	    	//System.out.println( "Compare textTolink.get( "+text+" ) = " + textTolink.get( text ) + " URLName = " + URLName + " Status-code = " + status );
-	    	
-	    	if( status == HttpURLConnection.HTTP_OK &&  textTolink.get( text ).equals( URLName ) )
-	    		return true;
-	    	else
-	    		return false;
-	
-	    } catch ( MalformedURLException e ) {
-	    	System.out.println( "MalformedURLException e = " + e.getMessage( ) );
-			return false;
-		} catch ( IOException e ) {
-			System.out.println( "IOException e = " + e.getMessage( ) );
-			return false;
-		}
-	    
-	}
-	
+    /**
+     * 
+     * @param filename
+     * @return
+     */
 	private boolean readFromFile( String filename ) {
 		textTolink = new HashMap< String , String >( );
 		try {
@@ -321,19 +260,6 @@ public class IndexSobrePage {
 			return false;
 		}
 	}
-    
-	
-	private boolean checkRedirect( int status ){
-		if (status != HttpURLConnection.HTTP_OK) {
-    		if (status == HttpURLConnection.HTTP_MOVED_TEMP
-    			|| status == HttpURLConnection.HTTP_MOVED_PERM
-    				|| status == HttpURLConnection.HTTP_SEE_OTHER)
-    		return true;
-    	}
-		return false;
-	}
-
-
 	
 }
 
