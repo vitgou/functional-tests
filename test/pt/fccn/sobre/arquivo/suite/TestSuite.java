897f50f47d616b0ddc104ac2fa2903c7d52d7807
jnobre <joaoanobre@gmail.com>
2017-07-13 18:12:42 +0100

    Add searchTest & sitemapteste

--- test/pt/fccn/sobre/arquivo/suite/TestSuite.java
+++ test/pt/fccn/sobre/arquivo/suite/TestSuite.java
@@ -19,10 +19,10 @@ import pt.fccn.sobre.arquivo.tests.SiteMapTest;
  *
  */
 @RunWith( Suite.class )
-@SuiteClasses( {  NavigationTest.class } )
-//TODO PublicationsTest.class  ,   , SearchTest.class , SiteMapTest.class
+@SuiteClasses( { SearchTest.class , SiteMapTest.class  } )
+//TODO PublicationsTest.class  ,   , 
 //TODO error SuggestionSiteTest.class , SiteMapTest.class
-//TODO ta feito CommonQuestionsTest.class , ExamplesTest.class , FooterTest.class  , NewsTest.class ,
+//TODO ta feito CommonQuestionsTest.class , ExamplesTest.class , FooterTest.class  , NewsTest.class , NavigationTest.class
 public class TestSuite {
 
 }
