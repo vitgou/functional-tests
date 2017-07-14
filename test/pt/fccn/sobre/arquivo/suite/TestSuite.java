08b953ba1c6baf519ab411f161e7e4c5ec4e5f18
jnobre <joaoanobre@gmail.com>
2017-07-14 12:02:23 +0100

    Run all tests sobre.arquivo.pt

--- test/pt/fccn/sobre/arquivo/suite/TestSuite.java
+++ test/pt/fccn/sobre/arquivo/suite/TestSuite.java
@@ -19,8 +19,8 @@ import pt.fccn.sobre.arquivo.tests.SiteMapTest;
  *
  */
 @RunWith( Suite.class )
-@SuiteClasses( { PublicationsTest.class } )
-//TODO CommonQuestionsTest.class , ExamplesTest.class , FooterTest.class  , NewsTest.class , NavigationTest.class, SearchTest.class , SiteMapTest.class
+@SuiteClasses( { CommonQuestionsTest.class , ExamplesTest.class , FooterTest.class  , NewsTest.class , NavigationTest.class, SearchTest.class , SiteMapTest.class, PublicationsTest.class } )
+//TODO 
 public class TestSuite {
 
 }
