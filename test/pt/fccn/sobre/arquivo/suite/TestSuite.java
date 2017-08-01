cd6c01d7f1d7a09fa881e14d3d17db675c3acfe5
jnobre <joaoanobre@gmail.com>
2017-08-01 13:58:00 +0100

    Run PublicationTest

--- test/pt/fccn/sobre/arquivo/suite/TestSuite.java
+++ test/pt/fccn/sobre/arquivo/suite/TestSuite.java
@@ -19,8 +19,8 @@ import pt.fccn.sobre.arquivo.tests.SiteMapTest;
  *
  */
 @RunWith( Suite.class )
-@SuiteClasses( {  NavigationTest.class, PublicationsTest.class } )
-//TODO CommonQuestionsTest.class , ExamplesTest.class , FooterTest.class  , NewsTest.class , SearchTest.class , SiteMapTest.class, 
+@SuiteClasses( { PublicationsTest.class } )
+//TODO CommonQuestionsTest.class , ExamplesTest.class , FooterTest.class  , NewsTest.class , SearchTest.class , SiteMapTest.class, NavigationTest.class 
 public class TestSuite {
 
 }
