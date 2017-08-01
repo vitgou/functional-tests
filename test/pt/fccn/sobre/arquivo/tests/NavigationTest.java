6b330528e8a68c50d446fb472f0ad6ed1421ff48
jnobre <joaoanobre@gmail.com>
2017-08-01 10:37:29 +0100

    Edit ExamplesPage and NavigationTest

--- test/pt/fccn/sobre/arquivo/tests/NavigationTest.java
+++ test/pt/fccn/sobre/arquivo/tests/NavigationTest.java
@@ -38,44 +38,44 @@ public class NavigationTest  extends WebDriverTestBaseParalell {
 			/*** Portuguese version ***/ 
 			/**************************/
 			NewsPage news = index.goToNewsPage( );
-			assertTrue("Failed The News Page Test in Portuguese", news.checkNewsLinks( "PT" ) );
+			assertTrue("[Nabigation Test] Failed The News Page Test in Portuguese", news.checkNewsLinks( "PT" ) );
 			PublicationsPage publications = index.goToPublicationsPage( "PT" );
 			ReportsPage reports = index.goToReportsPage( "PT" );
-			assertTrue("Failed The Reports Page Test in Portuguese", reports.checkReportsLinks( "PT" ) );
+			assertTrue("[Nabigation Test] Failed The Reports Page Test in Portuguese", reports.checkReportsLinks( "PT" ) );
 			NewsOnMediaPage newsonmedia = index.goToNewOnMediaPage( "PT" );
-			assertTrue("Failed The New on Media Page Test in Portuguese", newsonmedia.checkNewsLinks( "PT") );
+			assertTrue("[Nabigation Test] Failed The New on Media Page Test in Portuguese", newsonmedia.checkNewsLinks( "PT") );
 			AudioPage audio = index.goToAudioPage( "PT" );
-			assertTrue("Failed The Audio Page Test in Portuguese", audio.checkAudioLinks( "PT" ) );
+			assertTrue("[Nabigation Test] Failed The Audio Page Test in Portuguese", audio.checkAudioLinks( "PT" ) );
 			/*TODO to slow VideoPage video = index.goToVideoPage( "PT" );
 			assertTrue("Failed The Video Page Test in Portuguese", video.checkVideoLinks( "PT" ) );*/
 			PresentationsPage pres = index.goToPresentationsPage( "PT" );
-			assertTrue("Failed The Presentations Page Test in Portuguese", pres.checkPresentationLinks( "PT" ) );
+			assertTrue("[Nabigation Test] Failed The Presentations Page Test in Portuguese", pres.checkPresentationLinks( "PT" ) );
 			CollaboratePage coll = index.goToCollaboratePage( "PT" );
-			assertTrue("Failed The Collaborate Page Test in Portuguese", coll.checkCollaborateLinks( "PT" ) );
+			assertTrue("[Nabigation Test] Failed The Collaborate Page Test in Portuguese", coll.checkCollaborateLinks( "PT" ) );
 			AboutPage about = index.goToAboutPage( "PT" );
-			assertTrue("Failed The Collaborate Page Test in Portuguese", about.checkAboutLinks( "PT" ) );
+			assertTrue("[Nabigation Test] Failed The Collaborate Page Test in Portuguese", about.checkAboutLinks( "PT" ) );
 			System.out.println( "Success The Navigation Test in Portuguese" );
 			
 			/***********************/
 			/*** English version ***/ 
 			/***********************/
 			NewsPage newsEN = index.goToNewsPage( );
-			assertTrue("Failed The News Page Test in Portuguese", newsEN.checkNewsLinks( "EN" ) );
+			assertTrue("[Nabigation Test] Failed The News Page Test in Portuguese", newsEN.checkNewsLinks( "EN" ) );
 			PublicationsPage publicationsEN = index.goToPublicationsPage( "EN" );
 			ReportsPage reportsEN = index.goToReportsPage( "EN" );
-			assertTrue("Failed The Reports Page Test in Portuguese", reports.checkReportsLinks( "EN" ) );
+			assertTrue("[Nabigation Test] Failed The Reports Page Test in Portuguese", reports.checkReportsLinks( "EN" ) );
 			NewsOnMediaPage newsonmediaEN = index.goToNewOnMediaPage( "EN" );
-			assertTrue("Failed The New on Media Page Test in Portuguese", newsonmediaEN.checkNewsLinks( "EN") );
+			assertTrue("[Nabigation Test] Failed The New on Media Page Test in Portuguese", newsonmediaEN.checkNewsLinks( "EN") );
 			AudioPage audioEN = index.goToAudioPage( "EN" );
-			assertTrue("Failed The Audio Page Test in Portuguese", audioEN.checkAudioLinks( "EN" ) );
+			assertTrue("[Nabigation Test] Failed The Audio Page Test in Portuguese", audioEN.checkAudioLinks( "EN" ) );
 			//TODO to slow VideoPage video = index.goToVideoPage( "PT" );
 			//TODO assertTrue("Failed The Video Page Test in Portuguese", video.checkVideoLinks( "EN" ) );
 			PresentationsPage presEN = index.goToPresentationsPage( "EN" );
-			assertTrue("Failed The Presentations Page Test in Portuguese", presEN.checkPresentationLinks( "EN" ) );
+			assertTrue("[Nabigation Test] Failed The Presentations Page Test in Portuguese", presEN.checkPresentationLinks( "EN" ) );
 			CollaboratePage collEN = index.goToCollaboratePage( "EN" );
-			assertTrue("Failed The Collaborate Page Test in Portuguese", collEN.checkCollaborateLinks( "EN" ) );
+			assertTrue("[Nabigation Test] Failed The Collaborate Page Test in Portuguese", collEN.checkCollaborateLinks( "EN" ) );
 			AboutPage aboutEN = index.goToAboutPage( "EN" );
-			assertTrue("Failed The Collaborate Page Test in Portuguese", aboutEN.checkAboutLinks( "EN" ) );
+			assertTrue("[Nabigation Test] Failed The Collaborate Page Test in Portuguese", aboutEN.checkAboutLinks( "EN" ) );
 			System.out.println( "Success The Navigation Test in English" );
 	        
 		} catch( IOException e ) {
