1321b286e0ab63f503cc59696984ab16b0af2c18
DBicho <=>
2018-06-06 13:31:50 +0100

    testing environment

--- src/pt/fccn/saw/selenium/WebDriverTestBaseParalell.java
+++ src/pt/fccn/saw/selenium/WebDriverTestBaseParalell.java
@@ -175,7 +175,6 @@ public class WebDriverTestBaseParalell implements SauceOnDemandSessionIdProvider
         System.out.println("JSON: " + browsersJSON);
 
         JSONObject browsersJSONObject = new JSONObject("{browsers:"+browsersJSON+"}");
-        JSONArray browsersJSONArray = browsersJSONObject.getJSONArray("browsers");
 
         if(browsersJSON == null){
             System.out.println("You did not specify browsers, testing with firefox and chrome...");
@@ -183,6 +182,7 @@ public class WebDriverTestBaseParalell implements SauceOnDemandSessionIdProvider
             browsers.add(new String[]{"Windows 8.1", "46", "firefox", null, null});
         }
         else{
+            JSONArray browsersJSONArray = browsersJSONObject.getJSONArray("browsers");
             for (int i = 0; i < browsersJSONArray.length(); i++) {
               //TODO:: find names of extra properties for mobile Devices such as orientation and device name  
               JSONObject browserConfigs = browsersJSONArray.getJSONObject(i);  
