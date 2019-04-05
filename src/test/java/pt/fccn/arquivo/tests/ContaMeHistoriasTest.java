89f15b0f20fcb5f12a4d203dcd07fa8d11007117
Ivo Branco <ivo.branco@fccn.pt>
2019-04-05 09:58:46 +0100

    Add conta me historias test

---
+++ src/test/java/pt/fccn/arquivo/tests/ContaMeHistoriasTest.java
@@ -0,0 +1,33 @@
+package pt.fccn.arquivo.tests;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+
+import pt.fccn.saw.selenium.WebDriverTestBaseParalell;
+
+/**
+ *
+ * @author Ivo Branco <ivo.branco@fccn.pt>
+ *
+ */
+public class ContaMeHistoriasTest extends WebDriverTestBaseParalell {
+
+	public ContaMeHistoriasTest(String os, String version, String browser, String deviceName,
+			String deviceOrientation) {
+		super(os, version, browser, deviceName, deviceOrientation);
+	}
+
+	@Test
+	public void testImageSearchOneTerm() {
+		driver.get("http://contamehistorias.pt");
+
+		appendError(() -> {
+			assertTrue("Check element contains arquivo.pt reference",
+					driver.findElement(By.xpath("/html/body/footer/div/div/div[1]/div[1]/div")).getText()
+							.contains("Arquivo.pt"));
+		});
+
+	}
+}
