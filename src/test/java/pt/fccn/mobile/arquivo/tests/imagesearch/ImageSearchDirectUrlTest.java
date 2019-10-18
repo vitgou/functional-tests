40704deff6f992983439df8b97914cc74ae28ed2
Ivo Branco <ivo.branco@fccn.pt>
2019-10-18 14:42:55 +0100

    Improve image search direct url test with: no language, PT and EN language

--- src/test/java/pt/fccn/mobile/arquivo/tests/imagesearch/ImageSearchDirectUrlTest.java
+++ src/test/java/pt/fccn/mobile/arquivo/tests/imagesearch/ImageSearchDirectUrlTest.java
@@ -6,6 +6,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.junit.Test;
 import org.openqa.selenium.By;
@@ -20,16 +21,29 @@ import pt.fccn.arquivo.selenium.WebDriverTestBaseParalell;
  */
 public class ImageSearchDirectUrlTest extends WebDriverTestBaseParalell {
 
+	private static final String IMAGE_SEARCH_DIRECT_URL = "/images.jsp?l=en&size=all&type=&tools=off&safeSearch=on&query=fccn&btnSubmit=Search&dateStart=26%2F06%2F2007&dateEnd=27%2F06%2F2007";
+
 	public ImageSearchDirectUrlTest(String os, String version, String browser, String deviceName,
 			String deviceOrientation) {
 		super(os, version, browser, deviceName, deviceOrientation);
 	}
 
 	@Test
-	public void testImageSearchDirectUrl() throws Exception {
+	public void testImageSearchDirectUrlNoLanguage() throws Exception {
+		testImageSearchDirectUrl(testURL + IMAGE_SEARCH_DIRECT_URL, Optional.empty());
+	}
 
-		String url = testURL
-				+ "/images.jsp?l=en&size=all&type=&tools=off&safeSearch=on&query=fccn&btnSubmit=Search&dateStart=26%2F06%2F2007&dateEnd=27%2F06%2F2007";
+	@Test
+	public void testImageSearchDirectUrlPt() throws Exception {
+		testImageSearchDirectUrl(testURL + IMAGE_SEARCH_DIRECT_URL + "&l=pt", Optional.empty());
+	}
+
+	@Test
+	public void testImageSearchDirectUrlEn() throws Exception {
+		testImageSearchDirectUrl(testURL + IMAGE_SEARCH_DIRECT_URL + "&l=en", Optional.of("Images"));
+	}
+
+	private void testImageSearchDirectUrl(String url, Optional<String> imageButtonText) {
 		driver.get(url);
 
 		WebElement firstImage = driver.findElement(By.id("imageResults0"));
@@ -43,5 +57,11 @@ public class ImageSearchDirectUrlTest extends WebDriverTestBaseParalell {
 		appendError(() -> assertThat("Check image original origin/domain",
 				driver.findElement(By.xpath("//*[@id=\"imageResults0\"]/p[1]")).getText(),
 				containsString("sag.fccn.pt")));
+
+		if (imageButtonText.isPresent()) {
+			appendError(() -> assertEquals("Check page language by verifying images button text", imageButtonText.get(),
+					driver.findElementById("ImageButton").getText()));
+		}
 	}
+
 }
