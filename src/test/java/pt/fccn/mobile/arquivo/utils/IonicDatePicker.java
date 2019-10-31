8d592e8db2bd3d20864c9f18b9ea0e07d4f93152
Ivo Branco <ivo.branco@fccn.pt>
2019-10-31 11:04:53 +0000

    organize imports

--- src/test/java/pt/fccn/mobile/arquivo/utils/IonicDatePicker.java
+++ src/test/java/pt/fccn/mobile/arquivo/utils/IonicDatePicker.java
@@ -8,7 +8,6 @@ import java.util.stream.Stream;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.remote.RemoteWebDriver;
 
 /**
  * Class with utility method to deal with ionic date picker.
