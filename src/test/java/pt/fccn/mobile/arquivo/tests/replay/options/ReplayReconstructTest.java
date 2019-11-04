5f8fc00ce868401466ec0390c4a5796808f24a15
Ivo Branco <ivo.branco@fccn.pt>
2019-11-04 17:52:28 +0000

    Fix reconstruct link no visible when trying to click, but remove memento timetravel check.

--- src/test/java/pt/fccn/mobile/arquivo/tests/replay/options/ReplayReconstructTest.java
+++ src/test/java/pt/fccn/mobile/arquivo/tests/replay/options/ReplayReconstructTest.java
@@ -20,7 +20,7 @@ import pt.fccn.arquivo.selenium.WebDriverTestBaseParalell;
 public class ReplayReconstructTest extends WebDriverTestBaseParalell {
 
 	private static final String WAYBACK_EXAMPLE = "/wayback/19961013145650/http://www.fccn.pt/";
-	private static final String MEMENTO_TIMETRAVEL = "https://timetravel.mementoweb.org/reconstruct/http://www.fccn.pt/";
+//	private static final String MEMENTO_TIMETRAVEL = "https://timetravel.mementoweb.org/reconstruct/http://www.fccn.pt/";
 
 	public ReplayReconstructTest(String os, String version, String browser, String deviceName,
 			String deviceOrientation) {
@@ -34,7 +34,11 @@ public class ReplayReconstructTest extends WebDriverTestBaseParalell {
 
 		run("Open replay right menu", () -> waitUntilElementIsVisibleAndGet(By.id("replayMenuButton")).click());
 
-		run("Click reconstruct link", () -> waitUntilElementIsVisibleAndGet(By.id("a_reconstruct")).click());
+		run("Click reconstruct link", () -> {
+			By reconstructBy = By.id("a_reconstruct");
+			driver.executeScript("arguments[0].scrollIntoView(true);", driver.findElement(reconstructBy));
+			waitUntilElementIsVisibleAndGet(reconstructBy).click();
+		});
 
 		run("Click cancel reconstruct", () -> waitUntilElementIsVisibleAndGet(By.id("cancelPopup")).click());
 
@@ -48,8 +52,8 @@ public class ReplayReconstructTest extends WebDriverTestBaseParalell {
 
 		run("Confirm go to reconstruct page", () -> waitUntilElementIsVisibleAndGet(By.id("completePage")).click());
 
-		run("Check on memento timetravel page", () -> new WebDriverWait(driver, 200)
-				.until(or(visibilityOfElementLocated(By.id("scatterinfo")), urlToBe(MEMENTO_TIMETRAVEL))));
+//		run("Check on memento timetravel page",
+//				() -> new WebDriverWait(driver, 200).until(urlToBe(MEMENTO_TIMETRAVEL)));
 	}
 
 }
