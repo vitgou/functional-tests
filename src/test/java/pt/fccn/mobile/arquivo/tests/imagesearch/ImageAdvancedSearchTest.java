f1f59b6c64e7b6141619690f0ef92cac9c1d705e
Pedro-Gomes <pedo.gomes.fccn@gmail.com>
2020-03-20 15:09:14 +0000

    Fix AdvanceSearch

--- src/test/java/pt/fccn/mobile/arquivo/tests/imagesearch/ImageAdvancedSearchTest.java
+++ src/test/java/pt/fccn/mobile/arquivo/tests/imagesearch/ImageAdvancedSearchTest.java
@@ -50,13 +50,13 @@ public class ImageAdvancedSearchTest extends WebDriverTestBaseParalell {
 		
 		run("Close words box", () -> driver.findElement(By.xpath("//*[@id=\"words\"]/legend")).click());
 
-		run("Open start date picker", () -> driver.findElement(By.id("dateStart_top")).click());
-
+		run("Open start date picker", () -> driver.findElement(By.id("sliderCircleStart")).click());
+		
 		run("Insert 31 may 2010 on start date picker", () -> {
 			IonicDatePicker.changeTo(driver, LocalDate.of(2010, 5, 31));
 		});
 		
-		run("Open end date picker", () -> driver.findElement(By.id("dateEnd_top")).click());
+		run("Open end date picker", () -> driver.findElement(By.id("sliderCircleEnd")).click());
 		
 		run("Insert 1 jun 2012 on end date picker", () -> {
 			IonicDatePicker.changeTo(driver, LocalDate.of(2012, 6, 1));
