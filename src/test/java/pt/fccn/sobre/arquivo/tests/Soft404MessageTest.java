f01164caa2ced44b3d21e36663762262641d8903
Pedro-Gomes <pedo.gomes.fccn@gmail.com>
2020-05-22 15:20:14 +0100

    New test Soft404MessageTest

--- src/test/java/pt/fccn/sobre/arquivo/tests/Soft404MessageTest.java
+++ src/test/java/pt/fccn/sobre/arquivo/tests/Soft404MessageTest.java
@@ -47,19 +47,21 @@ public class Soft404MessageTest extends WebDriverTestBaseParalell {
 
 	public void soft404MessageTest(Locale locale) {
 		
-		driver.get(this.testURL + WAYBACK_404_PAGE_EXAMPLE);
-		
-		waitUntilElementIsVisibleAndGet(By.id("post-9818"));
-		
-		LocalizedString listText = new LocalizedString().pt("Visite uma versão anterior desta página em 2017-02-27 no Arquivo.pt").en("Browse a previous version of this page in 2017-02-28 on Arquivo.pt");
-		
-		appendError(() -> assertEquals("Verify text from Arquivo.pt link", listText.apply(locale),
-				driver.findElement(By.id("onArquivo")).getText()));
-		
-		List<WebElement> wes = driver.findElementsByXPath("//*[@id=\"onArquivo\"]");
-		WebElement we = wes.get(0);
-		String href = we.getAttribute("href");
-		assertEquals("Check link to wayback link", href, "https://arquivo.pt/wayback/20170227184149/https://sobre.arquivo.pt" + WAYBACK_404_PAGE_EXAMPLE);
+		if (!this.testURL.toLowerCase().contains("preprod.sobre.arquivo.pt")) {
+			driver.get(this.testURL + WAYBACK_404_PAGE_EXAMPLE);
+			
+			waitUntilElementIsVisibleAndGet(By.id("post-9818"));
+			
+			LocalizedString listText = new LocalizedString().pt("Visite uma versão anterior desta página em 2017-02-27 no Arquivo.pt").en("Browse a previous version of this page in 2017-02-28 on Arquivo.pt");
+			
+			appendError(() -> assertEquals("Verify text from Arquivo.pt link", listText.apply(locale),
+					driver.findElement(By.id("onArquivo")).getText()));
+			
+			List<WebElement> wes = driver.findElementsByXPath("//*[@id=\"onArquivo\"]");
+			WebElement we = wes.get(0);
+			String href = we.getAttribute("href");
+			assertEquals("Check link to wayback link", href, "https://arquivo.pt/wayback/20170227184149/https://sobre.arquivo.pt" + WAYBACK_404_PAGE_EXAMPLE);
+		}
 	}
 	
 	private void switchLanguage( ){
